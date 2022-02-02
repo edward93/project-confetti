93585ac852e2df7662b9374b2b747f8915a190a0
Edward <edward.chopuryan@gmail.com>
2022-02-02 17:28:53 -0500

    adds mouse move handling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,50 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import "../styles/App.scss";
-import partyPopper from "../images/party-popper.png";
+import partyPopperImg from "../images/party-popper.png";
+
+const dpr = window.devicePixelRatio || 1;
 
 const App = () => {
-  const canvasRef = useRef(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const partyPopperRef = useRef<HTMLDivElement>(null);
+
+  const registerListeners = () => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      canvas.addEventListener("mousemove", handleMouseMove);
+    }
+  };
+
+  const unregisterListeners = () => {
+    const canvas = canvasRef.current;
+    if (canvas) window.removeEventListener("mousemove", handleMouseMove);
+  };
+
+  const getDegAngel = (x0: number, y0: number, x1: number, y1: number): number => {
+    console.log(`x0 - ${x0}, y0 - ${y0}, x1 - ${x1}, y1 - ${y1}`);
+    const y = y1 - y0;
+    const x = x1 - x0;
+    return Math.atan2(y, x) * (180 / Math.PI);
+  };
+
+  const handleMouseMove = (e: MouseEvent) => {
+    const canvas = canvasRef.current;
+    let angle = 0;
+    // TODO: angle is a bit off need to be fixed
+    if (canvas) {
+      angle = getDegAngel(canvas.clientWidth / 2, canvas.clientHeight * 0.9, e.clientX * dpr, e.clientY * dpr);
+    }
+    if (partyPopperRef.current) {
+      partyPopperRef.current.style.transform = `translateX(0)rotate(${angle + 45}deg)`;
+    }
+  };
+
+  useEffect(() => {
+    registerListeners();
+    return () => {
+      unregisterListeners();
+    };
+  }, []);
 
   return (
     <div className="pc-main-content">
@@ -14,8 +55,8 @@ const App = () => {
           <span className="pc-intro-chinese">新年好</span>
         </div>
         <div className="pc-party-popper-container">
-          <div className="pc-party-popper">
-            <img src={partyPopper} alt="party popper" />
+          <div className="pc-party-popper" ref={partyPopperRef}>
+            <img src={partyPopperImg} alt="party popper" />
           </div>
         </div>
         <canvas ref={canvasRef} className="pc-canvas"></canvas>
